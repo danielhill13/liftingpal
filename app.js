b7cafb9a84e687925d73b6e8da53572a41f4d0bb
Daniel Hill <daniel.hill13@gmail.com>
2017-07-10 17:19:21 -0700

    figured out string replace

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express         = require('express'),
     app             = express();
 
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
--- public/js/lifting.js
+++ public/js/lifting.js
@@ -5,4 +5,9 @@ document.getElementById('add').addEventListener('click', toggleHide);
 
 function toggleHide(){
     newliftform.classList.toggle('hidden');
+    if(document.getElementById("add").textContent=="Add Exercise"){
+        document.getElementById("add").textContent="Cancel";
+        } else{
+            document.getElementById("add").textContent="Add Exercise";
+        }
 }
